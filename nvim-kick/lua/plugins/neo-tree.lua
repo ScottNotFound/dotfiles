e8f9e93c12c09f919d67c97f80d187773708b4bd
Scott Thiel <thiel.scott@gmail.com>
2026-09-18 18:12:18 -0400

    fix neo-tree

--- nvim-kick/lua/plugins/neo-tree.lua
+++ nvim-kick/lua/plugins/neo-tree.lua
@@ -1,73 +1,55 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  event = 'VimEnter',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-    {
-      's1n7ax/nvim-window-picker',
-      filter_rules = {
-        include_current_win = false,
-        autoselect_one = true,
-        bo = {
-          filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
-          buftype = { 'terminal', 'quickfix' },
-        },
-      },
-    },
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    {
-      '<leader>e',
-      function()
-        require('neo-tree.command').execute { toggle = true, dir = vim.uv.cwd() }
-      end,
-      desc = 'NeoTree Explorer',
-    },
-    {
-      '<leader>ge',
-      function()
-        require('neo-tree.command').execute { source = 'git_status', toggle = true }
-      end,
-      desc = 'Git Explorer',
-    },
-    {
-      '<leader>be',
-      function()
-        require('neo-tree.command').execute { source = 'buffers', toggle = true }
-      end,
-      desc = 'Buffer Explorer',
-    },
-  },
-  opts = {},
-  config = function()
-    require('neo-tree').setup {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['%'] = 'add',
-          ['<del>'] = 'delete',
-          ['l'] = 'open',
-          ['h'] = 'close_node',
-          ['E'] = 'expand_all_nodes',
-          ['C'] = 'close_all_nodes',
-          ['<F5>'] = 'refresh',
-        },
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-        },
-        hijack_netrw_behavior = 'open_current',
-      },
-    }
-  end,
+vim.pack.add {
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '3' },
+  { src = 'https://github.com/vim-lua/plenary.nvim' },
+  { src = 'https://github.com/MunifTanjim/nui.nvim' },
+--  '1n7ax/'unvim-window-picker',
 }
+
+vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+vim.keymap.set('n',
+  '<leader>e',
+  function()
+    require('neo-tree.command').execute { toggle = true, dir = vim.uv.cwd() }
+  end,
+  {  desc = 'NeoTree Explorer'}
+)
+vim.keymap.set('n',
+  '<leader>ge',
+  function()
+    require('neo-tree.command').execute { source = 'git_status', toggle = true }
+  end,
+  {desc = 'Git Explorer'}
+)
+vim.keymap.set('n',
+  '<leader>be',
+  function()
+    require('neo-tree.command').execute { source = 'buffers', toggle = true }
+  end,
+  {desc = 'Buffer Explorer'}
+)
+
+require('neo-tree').setup {
+  filesystem = {
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+        ['%'] = 'add',
+        ['<del>'] = 'delete',
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['E'] = 'expand_all_nodes',
+        ['C'] = 'close_all_nodes',
+        ['<F5>'] = 'refresh',
+      },
+    },
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+    },
+    hijack_netrw_behavior = 'open_current',
+  },
+}
+
